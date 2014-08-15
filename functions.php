f9fad16d7a1723c365710ccb80d514dd74219e8d
Lutz Selke <selke@tutech.de>
2014-08-15 14:00:07 +0200

    cleanup

--- functions.php
+++ functions.php
@@ -94,9 +94,7 @@ add_action( 'widgets_init', 'tuhh_institute_widgets_init' );
 function tuhh_institute_scripts() {
 	wp_enqueue_style( 'tuhh-institute-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'tuhh-institute-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'tuhh-institute-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+    // wp_enqueue_script( 'tuhh-institute-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- inc/jetpack.php
+++ inc/jetpack.php
@@ -5,15 +5,3 @@
  *
  * @package TUHH Institute
  */
-
-/**
- * Add theme support for Infinite Scroll.
- * See: http://jetpack.me/support/infinite-scroll/
- */
-function tuhh_institute_jetpack_setup() {
-	add_theme_support( 'infinite-scroll', array(
-		'container' => 'main',
-		'footer'    => 'page',
-	) );
-}
-add_action( 'after_setup_theme', 'tuhh_institute_jetpack_setup' );
