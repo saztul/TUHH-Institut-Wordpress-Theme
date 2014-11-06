8f915a4f21d4a01dc548c5cc4844a3eeb856b852
Lutz Selke <selke@tutech.de>
2014-11-06 08:43:02 +0100

    Design update

--- functions.php
+++ functions.php
@@ -106,7 +106,7 @@ add_action( 'widgets_init', 'tuhh_institute_widgets_init' );
  */
 function tuhh_institute_scripts() {
 	wp_enqueue_style( 'tuhh-institute-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'tuhh-institute-imported-style', get_template_directory_uri() . '/static/application-0101ff9dcf2852d3559e0b903c5e46d2.css' );
+	wp_enqueue_style( 'tuhh-institute-imported-style', get_template_directory_uri() . '/static/application-bd3e8c2b19c0a555d1965abedd06730f.css' );
 
     wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/static/assets/modernizr.js', array(), '20140815', true );
     wp_enqueue_script( 'tuhh-jquery-next-or-first', get_template_directory_uri() . '/js/jQuery_next_or_first.js', array('jquery'), '20140815', true );
