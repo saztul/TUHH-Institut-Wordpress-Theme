70addc5acd008e76af76f9e11f1b84c62d9ae0ba
Lutz Selke <selke@tutech.de>
2014-11-12 17:25:04 +0100

    Moved wp css adaptations out of style.css

--- functions.php
+++ functions.php
@@ -107,6 +107,7 @@ add_action( 'widgets_init', 'tuhh_institute_widgets_init' );
 function tuhh_institute_scripts() {
 	wp_enqueue_style( 'tuhh-institute-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'tuhh-institute-imported-style', get_template_directory_uri() . '/static/application-bd3e8c2b19c0a555d1965abedd06730f.css' );
+	wp_enqueue_style( 'tuhh-institute-wp-adaptations', get_template_directory_uri() . '/wp-adaptations.css' );
 
     wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/static/assets/modernizr.js', array(), '20140815', true );
     wp_enqueue_script( 'tuhh-jquery-next-or-first', get_template_directory_uri() . '/js/jQuery_next_or_first.js', array('jquery'), '20140815', true );
